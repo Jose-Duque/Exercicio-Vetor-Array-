f5d801b6257ed764626bd5ff183910f0e33be0db
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-01-04 13:41:55 -0300

    updated

--- src/app/program.java
+++ src/app/program.java
@@ -1,10 +1,41 @@
 package app;
 
+import java.util.Scanner;
+import entities.aluguelQuartos;
+
 public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		aluguelQuartos[] pensao = new aluguelQuartos[10];
+		
+		System.out.print("Hom many roows will be rented? ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Rent #" + i);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.nextLine();
+			System.out.print("Room: ");
+			int room = sc.nextInt();
+			
+			pensao[room] = new aluguelQuartos(name, email, room);
+		}
+		
+		System.out.println("Busy rooms:");
+		
+		for(int i = 0; i < pensao.length; i++) {
+			if(pensao[i] != null) {
+				
+				System.out.println(pensao[i].getRoom() + ": " + pensao[i].getName() + ", " + pensao[i].getEmail());
+			}
+		}
+		
+		sc.close();
 	}
 
 }
